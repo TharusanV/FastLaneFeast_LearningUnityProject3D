3159953183c549f9ef12a4ce71630186da77ff67
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2024-12-19 13:33:54 +0000

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float mouseSensitivity = 500f;
-
-    float xRotation = 0f;
-    float yRotation = 0f;
-
-    public float topClamp = -90f;
-    public float bottomClamp = 90f;
+    [SerializeField] //This allows us to see private variables in the inspector
+    private float moveSpeed = 7f;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        //Locking the cursor to the middle of the screen and making it invisible
-        Cursor.lockState = CursorLockMode.Locked;
+    private void Start(){
+
     }
 
     // Update is called once per frame
-    void Update(){
+    private void Update(){
+
+        //Get Input
+        Vector2 inputVector = new Vector2(0, 0); //Vector2 refers to the X and Y, typically used in 2d games
         
-        //Getting the mouse inputs
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; 
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; 
+        if (Input.GetKey (KeyCode.W)) {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey (KeyCode.S)) {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey (KeyCode.D)) {
+            inputVector.x = +1;
+        }
 
-        // Rotation around the x axis (Look up and down)
-        xRotation -= mouseY;
+        inputVector = inputVector.normalized; //If we didn't add this then the player would move faster diagonally than if they went in one direction, so normalising it makes it the same throughout
 
-        // Clamp the rotation
-        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
-
-        // Rotation around the y axis (Look left and right) 
-        yRotation += mouseX;
+        //Move object - Changing the transform directly is more easier than using physics
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); //To move backwards and forwards instead of using Y we use Z
+        transform.position += moveDir * moveSpeed * Time.deltaTime; //We multiply the Time.deltaTime with the direction and speed-multiplier to make the movemenet consistent no matter have fast a monitors frame rate
         
-        // Apply rotations to our transform 
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        //Rotate to look are the direction facing 
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed); //Smoothing with lerp so the rotation is more natural
     }
 }
