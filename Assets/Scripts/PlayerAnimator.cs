4106c2137139ae7a175f7f9530e4e55a6da381cd
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2024-12-19 22:29:44 +0000

    Animation, Movement/Controller

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+    private const String IS_WALKING = "IsWalking";
+
+    private Animator animator;
+
+    [SerializeField]
+    private PlayerController player;
+    
+    void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsPlayerWalking());
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,38 +8,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] //This allows us to see private variables in the inspector
     private float moveSpeed = 7f;
 
-    // Start is called before the first frame update
-    private void Start(){
+    [SerializeField] //This allows us to see private variables in the inspector
+    private PlayerInput playerInput;
 
+
+    private bool isWalking;
+
+    private void Awake(){
+        isWalking = false;
     }
 
     // Update is called once per frame
     private void Update(){
-
-        //Get Input
-        Vector2 inputVector = new Vector2(0, 0); //Vector2 refers to the X and Y, typically used in 2d games
-        
-        if (Input.GetKey (KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey (KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey (KeyCode.D)) {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized; //If we didn't add this then the player would move faster diagonally than if they went in one direction, so normalising it makes it the same throughout
-
+        Vector2 inputVector = playerInput.GetMovementVectorNormalized();
+       
         //Move object - Changing the transform directly is more easier than using physics
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); //To move backwards and forwards instead of using Y we use Z
         transform.position += moveDir * moveSpeed * Time.deltaTime; //We multiply the Time.deltaTime with the direction and speed-multiplier to make the movemenet consistent no matter have fast a monitors frame rate
         
+        isWalking = moveDir != Vector3.zero;
+
         //Rotate to look are the direction facing 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed); //Smoothing with lerp so the rotation is more natural
+    }
+
+    public bool IsPlayerWalking(){
+        return isWalking;
     }
 }
